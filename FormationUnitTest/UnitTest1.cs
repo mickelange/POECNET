0edb0a9544daf6add1381d3e49a4b133e75a9c61
mickelange <mcreusot@gmail.com>
2015-12-07 11:17:10 +0100

    Creation du projet

---
+++ FormationUnitTest/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FormationLibrary;
+
+namespace FormationUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AuthorTest()
+        {
+            Author a = new Author { Id = 1, Name = "Toto" };
+            Assert.AreEqual(1, a.Id);
+        }
+
+        [TestMethod]
+        public void BookTest()
+        {
+            Book b = new Book { Id = 1,Title ="Un titre",Category= Category.Roman};
+            Assert.AreEqual(1, b.Id);
+        }
+
+
+        [TestMethod]
+        public void PublisherTest()
+        {
+            Book b = new Book { Id = 1, Title = "Un titre", Category = Category.Roman, Publisher = new Publisher { Id = 2, Name = "Publisher" } };
+            Assert.AreEqual(2, b.Publisher.Id);
+        }
+
+        [TestMethod]
+        public void Author2Test()
+        {
+            Book b = new Book { Id = 1, Title = "Un titre", Category = Category.Roman };
+            b.Authors.Add(new Author { Id = 1, Name = "Toto" });
+            Assert.AreEqual(1, b.Authors.Count);
+        }
+
+
+    }
+}
